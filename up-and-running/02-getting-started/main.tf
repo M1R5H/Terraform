af9cb70a03c731142825a98764144e9661e61f9d
Matt Holmes <mattholmuk.ibm.com@Matts-MBP.broadband>
2020-06-08 16:16:32 +0100

    Updates

--- up-and-running/02-getting-started/main.tf
+++ up-and-running/02-getting-started/main.tf
@@ -3,8 +3,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "example02" {
-    ami                 = "ami-0ea3405d2d2522162"
+    ami                 = "ami-0c55b159cbfafe1f0"
     instance_type       = "t2.micro"
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
     tags = {
         Name = "terraform-example-02"
